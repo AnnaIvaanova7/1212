60fdefaf037206478218c46aaf1e303a224d7d6b
aaivanova7 <ivanna13122005@gmail.com>
2024-10-18 08:52:55 +0300

    init commit

--- src/Factory/creator/EllipseMaker.java
+++ src/Factory/creator/EllipseMaker.java
@@ -1,9 +1,9 @@
-package factory.creator;
+package Factory.creator;
 
 import java.awt.geom.Ellipse2D;
 import java.awt.geom.RectangularShape;
 
-public class EllipseMaker implements ShapeMaker {
+public class EllipseMaker implements factory.creator.ShapeMaker {
     @Override
     public RectangularShape buildShape() {
         return new Ellipse2D.Double();
--- src/Factory/creator/RectangleMaker.java
+++ src/Factory/creator/RectangleMaker.java
@@ -1,9 +1,9 @@
-package factory.creator;
+package Factory.creator;
 
 import java.awt.geom.Rectangle2D;
 import java.awt.geom.RectangularShape;
 
-public class RectangleMaker implements ShapeMaker {
+public class RectangleMaker implements factory.creator.ShapeMaker {
     @Override
     public RectangularShape buildShape() {
         return new Rectangle2D.Double();
